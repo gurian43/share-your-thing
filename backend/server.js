7ffe4a3247f1e7534ac361bebb95a4022fd2699c
lewdguri <gurix.cz@gmail.com>
2025-11-11 13:54:50 +0100

    session auth middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import MongoStore from 'connect-mongo';
 
 import connectToDatabase from './config/db.js';
 
@@ -12,7 +13,24 @@ const PORT = process.env.PORT || 5000;
 app.use(cors(
     {origin: process.env.CORS_ORIGIN || 'http://localhost:3000', credentials: true}
 ));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI,
+        collectionName: "sessions"
+    }),
+    cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 // 1 day
+    }
+}));
+
 
 app.listen(PORT, () => {
     connectToDatabase();
